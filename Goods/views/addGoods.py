8f63bab3226ac3dbe87059313091c1754d2a0c56
chaos <497386856@qq.com>
2016-06-08 22:28:06 +0800

    Goods.form - started

---
+++ Goods/views/addGoods.py
@@ -0,0 +1,12 @@
+#coding :utf-8
+from django.core.urlresolvers import reverse
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render_to_response,RequestContext
+from django.contrib.auth.decorators import login_required
+from website.common.CommonPaginator import SelfPaginator
+from UserManage.views.permission import PermissionVerify
+
+@login_required()
+@PermissionVerify()
+def AddGoods(request):
+    pass
